97aa0b0a4e687339943d9866d9637a49ef7b386f
Grauly <nils.dellemann@web.de>
2024-01-02 22:52:45 +0100

    Inserted Template

---
+++ src/main/java/grauly/sevenslasher/client/SevenSlasherClient.java
@@ -0,0 +1,10 @@
+package grauly.sevenslasher.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SevenSlasherClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
